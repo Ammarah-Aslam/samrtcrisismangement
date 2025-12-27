1db06832e3302910b6b71caf30a8bab38ae25a8a
Ammarah-Aslam <ammarahaslam912@gmail.com>
2025-12-27 20:36:24 +0500

    Update incidentManager.h

--- incidentManager.h
+++ incidentManager.h
@@ -1,20 +1,26 @@
-#ifndef INCIDENT_MANAGER_H
-#define INCIDENT_MANAGER_H
+#ifndef INCIDENTMANAGER_H
+#define INCIDENTMANAGER_H
 
 #include <string>
 #include <vector>
 using namespace std;
 
+// Incident Node for Linked List
+
 struct IncidentNode {
-    string type, location, description;
+    string type;
+    string location;
+    string description;
     int severity;
     IncidentNode* next;
 
-    IncidentNode(string t, string l, string d, int s) {
-        type = t; location = l; description = d; severity = s;
-        next = nullptr;
+    IncidentNode(string t, string l, string d, int s)
+        : type(t), location(l), description(d), severity(s), next(nullptr) {
     }
 };
+
+
+// BST Node (Search incidents by type)
 
 struct BSTNode {
     string type;
@@ -22,34 +28,58 @@ struct BSTNode {
     BSTNode* left;
     BSTNode* right;
 
-    BSTNode(string t) {
-        type = t;
-        left = right = nullptr;
-    }
+    BSTNode(string t) : type(t), left(nullptr), right(nullptr) {}
 };
+
+struct PQitem {                              // Priority Queue (Min-Heap based on severity)
+
+    int severity;
+    IncidentNode* incident;
+};
+
+// IncidentManager CLASS
 
 class IncidentManager {
 private:
+
     IncidentNode* head;
+    vector<IncidentNode*> stackHistory;
+    vector<IncidentNode*> queueList;
+    vector<PQitem> minHeap;
+
     BSTNode* root;
 
 public:
+
+    // ----------- Constructor -----------
     IncidentManager();
 
-    // Linked List
-    void addIncident(string, string, string, int);
-    void deleteIncident(string);
+    void addIncident(string t, string l, string d, int s);
+    void deleteIncident(string t);
     void displayLL();
+
+    void pushHistory(IncidentNode* node);
+    IncidentNode* popHistory();
+
+    void enqueueIncident(IncidentNode* node);
+    IncidentNode* dequeueIncident();
+
+    void insertPQ(IncidentNode* node, int severity);
+    IncidentNode* extractMinPQ();
+    void heapifyUp(int index);
+    void heapifyDown(int index);
+
+    BSTNode* insertBST(BSTNode* root, IncidentNode* node);
+    vector<IncidentNode*> searchBST(string type);
+    void inorderBST(BSTNode* root);
+
     int totalIncidents();
+    // ADD ONLY
+    void loadFromCSV(string filename);
 
-    // BST
-    BSTNode* insertBST(BSTNode*, IncidentNode*);
-    vector<IncidentNode*> searchBST(string);
-    void inorderBST(BSTNode*);
-
-    // FILE LOADING ⭐
-    void loadFromFile(string filename);
 };
 
-#endif
+#endif#pragma once
+
+
 
