73a8e40742141ef03927e6892616bb321e2d03d9
zaynab-ox <zainabss987@gmail.com>
2025-12-28 23:52:30 +0500

    minor fixing

--- RoutingManager.cpp
+++ RoutingManager.cpp
@@ -1,23 +1,32 @@
 #include "RoutingManager.h"
 
 RoutingManager::RoutingManager(int locations)
-    : cityGraph(locations) {
+    : cityGraph(locations)
+{
+    // constructor body intentionally left empty
 }
 
-void RoutingManager::createCityMap() {
+void RoutingManager::createCityMap()
+{
     cityGraph.addEdge(0, 1, 4);
     cityGraph.addEdge(0, 2, 2);
+
     cityGraph.addEdge(1, 2, 1);
     cityGraph.addEdge(1, 3, 5);
+
     cityGraph.addEdge(2, 3, 8);
+
     cityGraph.addEdge(3, 4, 3);
 }
 
-void RoutingManager::showRoutes(int source) {
+void RoutingManager::showRoutes(int source)
+{
     cityGraph.dijkstra(source);
 }
 
-void RoutingManager::traverseCity(int source) {
+void RoutingManager::traverseCity(int source)
+{
     cityGraph.BFS(source);
     cityGraph.DFS(source);
 }
+
