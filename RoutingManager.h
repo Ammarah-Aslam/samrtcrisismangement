ad456153c0bbc66a1bbd8664bc4770fb94081120
Ammarah-Aslam <ammarahaslam912@gmail.com>
2025-12-30 00:04:31 +0500

    Update RoutingManager.h

--- RoutingManager.h
+++ RoutingManager.h
@@ -2,18 +2,19 @@
 #define ROUTINGMANAGER_H
 
 #include "Graph.h"
+#include <string>
 
 class RoutingManager {
 private:
-    Graph cityGraph;
+  Graph cityGraph;
 
 public:
-    RoutingManager(int locations);
+  RoutingManager(int locations);
 
-    void createCityMap();
-    void showRoutes(int source);
-    void traverseCity(int source);
+  void createCityMap();
+  std::string showRoutes(int source);
+  std::string doBFS(int source);
+  std::string doDFS(int source);
 };
 
 #endif
-#pragma once
